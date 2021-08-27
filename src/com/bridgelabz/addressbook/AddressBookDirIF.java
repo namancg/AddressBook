bcf9d58b6b44b84f8002658adbf0afdd680cda7e
namancg <naman.c@perfios.com>
2021-08-28 02:29:10 +0530

    ADDED: interface for functions

---
+++ src/com/bridgelabz/addressbook/AddressBookDirIF.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.addressbook;
+
+public interface AddressBookDirIF {
+	public void operationSystem();
+	public void addAddressBook();
+	public void displaySystemContents();
+	public void editAddress();
+}
