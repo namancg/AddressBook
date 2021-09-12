d8f3d9aa7170dd8123f61f0ee5f50aa3d1d24400
namancg <naman.c@perfios.com>
2021-09-12 22:00:06 +0530

    REFACTORED: methods to display people from a region

--- src/com/bridgelabz/addressbook/AddressBookDirIF.java
+++ src/com/bridgelabz/addressbook/AddressBookDirIF.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.addressbook;
-
+import java.util.*;
 public interface AddressBookDirIF {
 	public void operationSystem();
 	public void addAddressBook();
 	public void displaySystemContents();
 	public void editAddress();
+	public void searchByCity();
+	public void searchByState();
+	public void displayPeopleByRegion(HashMap<String, ArrayList<PersonContact>> listToDisplay);
 }
