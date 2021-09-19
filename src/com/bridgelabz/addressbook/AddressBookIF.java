e9545522ea1a9f55a4c69cde58bfc0a460bd0df1
namancg <naman.c@perfios.com>
2021-09-19 18:51:26 +0530

    ADDED: methods to read and write to a file

--- src/com/bridgelabz/addressbook/AddressBookIF.java
+++ src/com/bridgelabz/addressbook/AddressBookIF.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.addressbook;
 
+import java.util.List;
+
 public interface AddressBookIF {
 	public void startOperation();
 	public void addContact();
 	public void displayContents();
 	public void editPerson();
 	public void deletePerson();
+	public void writeToAddressBookFile();
+	public List<String> readDataFromFile();
 	
 }
