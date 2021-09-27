97ff0f947f3fa784943583922aacd9e138a3d3c8
namancg <naman.c@perfios.com>
2021-09-28 01:41:18 +0530

    REFACTORED: prepared statements to read contatcs

--- src/main/java/com/bridgelabz/addressbookproblem/AddressBookDBService.java
+++ src/main/java/com/bridgelabz/addressbookproblem/AddressBookDBService.java
@@ -1,6 +1,4 @@
 package com.bridgelabz.addressbookproblem;
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -11,30 +9,15 @@ import java.util.List;
 
 public class AddressBookDBService
 {
-	public List<PersonContact> readData() 
-	{
-		String sql = "select * from contact_details c inner join addressBook_contact ac on ac.contact_id=c.contact_id";
-		List<PersonContact> contactList = new ArrayList<>();
-		try {
-			Connection connection = this.getConnection();
-			Statement statement = (Statement) connection.createStatement();
-			ResultSet result = ((java.sql.Statement) statement).executeQuery(sql);
-			while(result.next()) {	
-				PersonContact person  = new PersonContact();
-				person.setFirstName(result.getString("first_name"));
-				person.setLastName(result.getString("last_name"));
-				person.setnumber(result.getString("phone_number"));
-				person.setEmail(result.getString("email"));
-				contactList.add(person);
-			}
-			connection.close();
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return contactList;
+	private static AddressBookDBService addressBookDBService;
+	private java.sql.PreparedStatement readContactPreparedStatement;
+	private AddressBookDBService() {
+		
 	}
-	
+	public static AddressBookDBService getDBInstance() {
+		addressBookDBService = new AddressBookDBService();
+		return addressBookDBService;
+	}
 	public Connection getConnection() throws SQLException
 	{
 		String jdbcURL = "jdbc:mysql://localhost:3306/addressBook_service?userSSL=false";
@@ -47,4 +30,55 @@ public class AddressBookDBService
 		return connection;
 	
 	}
+	public List<PersonContact> readContacts(String addressBookName){
+		ResultSet resultSet;
+		if(readContactPreparedStatement==null)
+			this.preparedStatementToReadContacts();
+		try {
+			readContactPreparedStatement.setString(1, addressBookName);
+			resultSet=readContactPreparedStatement.executeQuery();
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		}
+		
+		return getContactList(resultSet);
+	}
+	public List<PersonContact> getContactList(ResultSet resultSet){
+		List<PersonContact> contactsList=new ArrayList<PersonContact>();
+		try {
+			while(resultSet.next()) {
+				contactsList.add(new PersonContact(null, resultSet.getString("first_name"), resultSet.getString("last_name"), resultSet.getString("city"), resultSet.getString("address"), resultSet.getString("state"), resultSet.getInt("zip"),new Long(resultSet.getLong("phone_number")).intValue(), resultSet.getString("email"), null));
+			}
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		}
+		return contactsList;
+
+	}
+	public void writeAddressBookDB(PersonContact contact,String addressBookName) {
+		String insertQueryString="INSERT INTO contact_details values("+contact.getContactId()+",\""+contact.getFirstName()+"\",\""+contact.getLastName()+"\",\""+contact.getAddress()+"\",\""+contact.getEmail()+"\","+contact.getPlace().getPlaceId()+",\""+contact.getPhoneNumber()+"\")";
+		String insertPlaceQueryString="INSERT into place values("+contact.getPlace().getPlaceId()+",\""+contact.getCity()+"\",\""+contact.getZipCode()+"\",\""+contact.getState()+"\")";
+		String insertIntermediateTableString="insert into addressBook_contact values((select book_id from address_book where book_name=\""+addressBookName+"\"),(select contact_id from contact_details where first_name=\""+contact.getFirstName()+"\"))";
+		try {
+			Connection connection=this.getConnection();
+			System.out.println("connected");
+			Statement statement=connection.createStatement();
+			statement.executeUpdate(insertPlaceQueryString);
+			statement.executeUpdate(insertQueryString);
+			statement.executeUpdate(insertIntermediateTableString);
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	private void preparedStatementToReadContacts() {
+	    try {
+	        Connection connection = this.getConnection();
+	    	String sql="SELECT first_name,last_name,city,address,state,zip,phone_number,email from address_book a,contact_details c,addressBook_contact ac,place p  where p.place_id=c.place_id and a.book_id=ac.book_id and c.contact_id=ac.contact_id and book_name=?";
+	       readContactPreparedStatement=connection.prepareStatement(sql);
+	    } catch (Exception e) {
+	        throw new DBException(e.getMessage());
+	    }
+	}
+	
 }
