77ee31c11a2c05fbd1fc2641f62028c71c7e6a1f
namancg <naman.c@perfios.com>
2021-09-28 01:44:35 +0530

    ADDED: test cases to check operations of DB

--- src/test/java/com/bridgelabz/addressbookproblem/AddressBookTest.java
+++ src/test/java/com/bridgelabz/addressbookproblem/AddressBookTest.java
@@ -18,7 +18,7 @@ static AddressBook addressBook = new AddressBook();
 		String firstName = "NAMAN";
 		String lastName = "CHANDRA";
 		String email = "ncg@gmail.com";
-		String phoneNumber = "953816996";
+		long phoneNumber = 953816996;
 		String city = "Bangalore";
 		String state = "Karnataka";
 		long zipCode = 560004;
@@ -29,7 +29,6 @@ static AddressBook addressBook = new AddressBook();
 		address.setCity(city);
 		address.setState(state);
 		address.setZip(zipCode);
-		person.setAddress(address);
 		addressBook.addContact(firstName, person);
 		
 	}
@@ -39,7 +38,7 @@ static AddressBook addressBook = new AddressBook();
 		AddressBookFileIO addressFileIO = new AddressBookFileIO();
 		addressFileIO.writeToAddressBookFile("BookNo1.txt", addressBook.contactList);
 		addressFileIO.printData("BookNo1.txt");
-		Assert.assertEquals(8, addressFileIO.countEntries("BookNo1.txt"));
+		Assert.assertEquals(6, addressFileIO.countEntries("BookNo1.txt"));
 		
 	}
 	
@@ -49,14 +48,26 @@ static AddressBook addressBook = new AddressBook();
 		AddressBookFileIO addressFileIO = new AddressBookFileIO();
 		List<String> entries = addressFileIO.readDataFromFile("BookNo1.txt");
 		long countEntries = entries.size();
-		Assert.assertEquals(8, countEntries);
+		Assert.assertEquals(6, countEntries);
 	}
 	@Test
-	public void givenAdressBookDB_WhenRetrived_ShouldatchAddressBookCount() 
-	{
-		AddressBookDir addressBook = new AddressBookDir();
-		List<PersonContact> count = addressBook.readData(IOService.DB_IO);
-		Assert.assertEquals(8, count.size());
+	public void givenAddressBookName_CheckIfAllContactsAreFetched(){
+		AddressBookDir addressBook=new AddressBookDir();
+		int AddressBookSize=addressBook.readData("ADDRESS_BOOK_1");
+		Assert.assertEquals(4,	AddressBookSize);
+	}
+	@Test
+	public void insertContactToGivenAddressBook_checkIfInserted() {
+		AddressBookDir addressBook=new AddressBookDir();
+		int AddressBookSize=addressBook.readData("ADDRESS_BOOK_2");
+		Assert.assertEquals(3,	AddressBookSize);
+		System.out.println(AddressBookSize);
+		PersonContact contact= new PersonContact(999, "LEWIS", "HAMILTON", "CHARLESTOWN", "ENGLAND", "UNITED KINGDOM",0101, 99292939,"LEWIS@gmail.com", 99);
+		addressBook.writeAddressBookDB(contact,"ADDRESS_BOOK_2");
+		int updatedSize=addressBook.readData("ADDRESS_BOOK_2");
+		System.out.println(updatedSize);
+		Assert.assertEquals(AddressBookSize,updatedSize);
+		
 	}
 	
 	
