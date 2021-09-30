4eba97dbc4be641eb88cb7fb632aa376569aab3c
namancg <naman.c@perfios.com>
2021-09-30 23:32:21 +0530

    ADDED: setters getters and constructors for contact class

---
+++ src/main/java/com/bridgelabz/addressbookproblem/Contact.java
@@ -0,0 +1,76 @@
+package com.bridgelabz.addressbookproblem;
+
+public class Contact {
+	private int contactID;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String email;
+	private String added;
+	
+	public Contact(int contactID, String firstName, String lastName, String phoneNumber, String email,String added) {
+		super();
+		this.contactID = contactID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.added = added;
+	}
+	public Contact() {
+		
+	}
+	public Contact(String firstName, String lastName, String phoneNumber, String email)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+	
+	public String getAdded() {
+		return added;
+	}
+	public void setAdded(String added) {
+		this.added = added;
+	}
+	public int getContactID() {
+		return contactID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setContactID(int contactID) {
+		this.contactID = contactID;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
