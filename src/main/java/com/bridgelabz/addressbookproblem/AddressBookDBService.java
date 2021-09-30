e1f5be333d14208e4fcedc2ea6b1d40f7c97ea85
namancg <naman.c@perfios.com>
2021-09-30 23:30:49 +0530

    REFACTORED: methods to add a new contact to DB

--- src/main/java/com/bridgelabz/addressbookproblem/AddressBookDBService.java
+++ src/main/java/com/bridgelabz/addressbookproblem/AddressBookDBService.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.addressbookproblem;
+
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
@@ -9,124 +10,343 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AddressBookDBService
-{
+public class AddressBookDBService {
 	private static AddressBookDBService addressBookDBService;
-	private java.sql.PreparedStatement readContactPreparedStatement;
-	 private PreparedStatement contactAddedGivenRangeStatement;
-	 private PreparedStatement contactsInGivenCityOrStateStatement;
-	private AddressBookDBService() {
-		
+	private PreparedStatement addressDataStatement;
+	private static List<PersonContact> addressList;
+
+	AddressBookDBService() {
+
 	}
+
+	public List<PersonContact> readData() {
+		String sql = "SELECT * FROM contacts";
+		List<PersonContact> contactList = new ArrayList<>();
+		try {
+			Connection connection = this.getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			while (result.next()) {
+				PersonContact person = new PersonContact();
+				person.setFirstName(result.getString("firstName"));
+				person.setLastName(result.getString("lastName"));
+				person.setPhoneNumber(result.getString("phoneNumber"));
+				person.setEmail(result.getString("email"));
+				contactList.add(person);
+			}
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+	}
+
 	public static AddressBookDBService getDBInstance() {
 		addressBookDBService = new AddressBookDBService();
 		return addressBookDBService;
 	}
-	public Connection getConnection() throws SQLException
-	{
+
+	public Connection getConnection() throws SQLException {
 		String jdbcURL = "jdbc:mysql://localhost:3306/addressBook_service?userSSL=false";
 		String userName = "root";
 		String password = "naman1999";
 		Connection connection;
-		System.out.println("Connecting to database:"+jdbcURL);
-		connection =  (Connection) DriverManager.getConnection(jdbcURL,userName,password);
-		System.out.println("Connection is successful"+connection);
+		System.out.println("Connecting to database:" + jdbcURL);
+		connection = (Connection) DriverManager.getConnection(jdbcURL, userName, password);
+		System.out.println("Connection is successful" + connection);
 		return connection;
-	
-	}
-	public List<PersonContact> readContacts(String addressBookName){
-		ResultSet resultSet;
-		if(readContactPreparedStatement==null)
-			this.preparedStatementToReadContacts();
-		try {
-			readContactPreparedStatement.setString(1, addressBookName);
-			resultSet=readContactPreparedStatement.executeQuery();
-		} catch (SQLException e) {
-			throw new DBException(e.getMessage());
-		}
-		
-		return getContactList(resultSet);
-	}
-	public List<PersonContact> getContactList(ResultSet resultSet){
-		List<PersonContact> contactsList=new ArrayList<PersonContact>();
-		try {
-			while(resultSet.next()) {
-				contactsList.add(new PersonContact(null, resultSet.getString("first_name"), resultSet.getString("last_name"), resultSet.getString("city"), resultSet.getString("address"), resultSet.getString("state"), resultSet.getInt("zip"),new Long(resultSet.getLong("phone_number")).intValue(), resultSet.getString("email"), null));
-			}
-		} catch (SQLException e) {
-			throw new DBException(e.getMessage());
-		}
-		return contactsList;
 
 	}
-	public void writeAddressBookDB(PersonContact contact,String addressBookName) {
-		String insertQueryString="INSERT INTO contact_details values("+contact.getContactId()+",\""+contact.getFirstName()+"\",\""+contact.getLastName()+"\",\""+contact.getAddress()+"\",\""+contact.getEmail()+"\","+contact.getPlace().getPlaceId()+",\""+contact.getPhoneNumber()+"\")";
-		String insertPlaceQueryString="INSERT into place values("+contact.getPlace().getPlaceId()+",\""+contact.getCity()+"\",\""+contact.getZipCode()+"\",\""+contact.getState()+"\")";
-		String insertIntermediateTableString="insert into addressBook_contact values((select book_id from address_book where book_name=\""+addressBookName+"\"),(select contact_id from contact_details where first_name=\""+contact.getFirstName()+"\"))";
+	public List<Contact> readContactData() {
+		String sql = "SELECT * FROM contacts";
+		List<Contact> contactList = new ArrayList<>();
 		try {
-			Connection connection=this.getConnection();
-			System.out.println("connected");
-			Statement statement=connection.createStatement();
-			statement.executeUpdate(insertPlaceQueryString);
-			statement.executeUpdate(insertQueryString);
-			statement.executeUpdate(insertIntermediateTableString);
+			Connection connection = this.getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			while(result.next()) {
+				Contact person  = new Contact();
+				person.setFirstName(result.getString("firstName"));
+				person.setLastName(result.getString("lastName"));
+				person.setPhoneNumber(result.getString("phoneNumber"));
+				person.setEmail(result.getString("email"));
+				contactList.add(person);
+			}
+			connection.close();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+	}
+
+	public Contact addContactToAddress(String firstName, String lastName, String phoneNumber, String email) {
+		int contactID = -1;
+		Contact contactData = null;
+		String sql = String.format(
+				"INSERT INTO contacts(firstname,lastname,phoneNumber,email) VALUES('%s','%s','%s','%s')", firstName,
+				lastName, phoneNumber, email);
+		try {
+			Connection connection = this.getConnection();
+			Statement statement = connection.createStatement();
+			int result = statement.executeUpdate(sql, statement.RETURN_GENERATED_KEYS);
+			if (result == 1) {
+				ResultSet resultSet = statement.getGeneratedKeys();
+				if (resultSet.next())
+					contactID = resultSet.getInt(1);
+			}
+			connection.close();
+			contactData = new Contact(firstName, lastName, phoneNumber, email);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println(contactData);
+		return contactData;
+	}
+
+	public List<Contact> readContactDetails(String firstName, String lastName, String phoneNumber, String email) {
+
+		String sqlStatement = "SELECT * FROM contacts JOIN address ON contacts.address_id = address.address_id;";
+		List<Contact> contactsList = new ArrayList<>();
+
+		try (Connection connection = getConnection();) {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sqlStatement);	
+			contactsList = getContactList(resultSet);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactsList;
+	}
+
+	public List<Contact> readContactAddressData(String city, String state) {
+		String sqlStatement = String.format(
+				"SELECT count(c.contact_id) from contacts c , address a WHERE c.contact_id =a.contact_id  and a.city = '%s' and a.state ='%s'",
+				city, state);
+		List<Contact> contactList = new ArrayList<>();
+
+		try (Connection connection = getConnection();) {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sqlStatement);
+			contactList = this.getContactList(resultSet);
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		}
+		return contactList;
+	}
+
+	public List<Contact> getContactList(ResultSet resultSet) {
+		List<Contact> contactList = new ArrayList<>();
+		try {
+			while (resultSet.next()) {
+				String firstName = resultSet.getString("firstName");
+				String lastName = resultSet.getString("lastName");
+				String phoneNumber = resultSet.getString("phoneNumber");
+				String email = resultSet.getString("email");
+				contactList.add(new Contact(firstName, lastName, phoneNumber, email));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+	}
+	public List<Contact> getContactData(String name) {
+		List<Contact> contactList = null;
+		if(this.addressDataStatement == null) {
+			this.prepareStatementForContactData();
+		}
+		try {
+			addressDataStatement.setString(1, name);
+			ResultSet resultSet = addressDataStatement.executeQuery();
+			contactList = this.getAddressContactData(resultSet);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+	}
+	public List<Contact> getAddressContactData(ResultSet resultSet) {
+		List<Contact> contactList = new ArrayList<>();
+		try {
+			while(resultSet.next()) {
+				String firstName = resultSet.getString("firstName");
+				String lastName = resultSet.getString("lastName");
+				String phoneNumber = resultSet.getString("phoneNumber");
+				String email = resultSet.getString("email");
+				contactList.add(new Contact(firstName, lastName, phoneNumber,email));
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+		
+	}
+	
+
+	public Contact addNewContactToContacts(String firstName, String lastName, String phoneNumber, String email,
+			String added) {
+
+		Connection connection = null;
+		Contact contactPerson = null;
+
+		try {
+			connection = this.getConnection();
+			connection.setAutoCommit(false);
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		}
+		try (Statement statement = connection.createStatement()) {
+
+			String sql = String.format(
+					"INSERT INTO contacts (firstName, lastName, phoneNumber, email, added) VALUES ('%s', '%s', '%s', '%s', '%s');",
+					firstName, lastName, phoneNumber, email, added);
+			statement.executeUpdate(sql);
+			contactPerson = new Contact(firstName, lastName, email, phoneNumber);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactPerson;
+	}
+
+	public List<Contact> getContactsBasedOnStartDateUsingPreparedStatement(String startDate, String endDate) {
+
+		String sqlStatement = String.format("SELECT * FROM contacts WHERE date_added BETWEEN '%s' AND '%s';", startDate,
+				endDate);
+		List<Contact> contactList = new ArrayList<>();
+
+		try (Connection connection = getConnection();) {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sqlStatement);
+			contactList = this.getContactList(resultSet);
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		}
+		return contactList;
+	}
+
+	public List<Contact> getContactDetailsBasedOnCityUsingStatement(String city) {
+
+		String sqlStatement = String.format(
+				"SELECT * FROM address where city='%s';",city );
+		List<Contact> contactList = new ArrayList<>();
+
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sqlStatement);
+			contactList = getContactDetails(resultSet);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+	}
+
+	public List<Contact> getContactDetailsBasedOnStateUsingStatement(String state) {
+
+		String sqlStatement = String.format(
+				"SELECT * FROM address WHERE state = '%s';", state);
+		List<Contact> contactList = new ArrayList<>();
+
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sqlStatement);
+			contactList = getContactDetails(resultSet);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+	}
+	public List<PersonContact> getPersonDetailsBasedOnNameUsingStatement(String name) {
+		String sqlStatement = String.format("SELECT * FROM contacts WHERE firstName  = '%s';",name);
+		List<PersonContact> addressList = new ArrayList<>();
+				
+		try (Connection connection = this.getConnection();){
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sqlStatement);
+			addressList = this.getAddressData(resultSet);
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		return addressList;
+	}
+	public int updateContact(String firstName, String lastName, String phoneNumber, String email) 
+	{
+		String sqlString = String.format("update contacts set lastName ='%s',phoneNumber ='%s',email='%s' where firstName= '%s';",lastName,phoneNumber,email,firstName);
+		try(Connection connection = this.getConnection()) {
+			Statement statement = connection.createStatement();
+			return statement.executeUpdate(sqlString);
+		}
+		catch(SQLException e) 
+		{
+			e.printStackTrace();
+		}return 0;
+	}
+	private List<PersonContact> getAddressData(ResultSet resultSet) {
+		
+		addressList = new ArrayList<>();
 			
-		}catch (Exception e) {
+			try {
+				while(resultSet.next()) {
+					String firstName = resultSet.getString("firstname");
+					String lastName = resultSet.getString("lastName");
+					String address = resultSet.getString("address");
+					String city  = resultSet.getString("city");
+					String state   = resultSet.getString("state");
+					String phoneNumber   = resultSet.getString("phoneNumber");
+					int pinCode   = resultSet.getInt("pinCode");
+					String email   = resultSet.getString("email");
+					addressList.add(new PersonContact(firstName,lastName, address, city,state,pinCode,phoneNumber,email));
+					
+				}
+			}
+			catch(SQLException e) {
+				e.printStackTrace();
+			}
+			return addressList;
+			
+		}
+
+	private List<Contact> getContactDetails(ResultSet resultSet) {
+
+		List<Contact> contactList = new ArrayList<>();
+
+		try {
+			while (resultSet.next()) {
+				int contactId = resultSet.getInt("contact_id");
+				String firstName = resultSet.getString("firstName");
+				String lastName = resultSet.getString("lastName");
+				String phoneNumber = resultSet.getString("phoneNumber");
+				String email = resultSet.getString("email");
+				String added = resultSet.getString("added");
+				contactList.add(new Contact(contactId, firstName, lastName, phoneNumber, email, added));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+
+	}
+
+	private void prepareStatementForAddressData() {
+
+		try {
+			Connection connection = this.getConnection();
+			String sqlStatement = "SELECT * FROM address_Book WHERE name = ?;";
+			addressDataStatement = connection.prepareStatement(sqlStatement);
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	private void preparedStatementToReadContacts() {
-	    try {
-	        Connection connection = this.getConnection();
-	    	String sql="SELECT first_name,last_name,city,address,state,zip,phone_number,email from address_book a,contact_details c,addressBook_contact ac,place p  where p.place_id=c.place_id and a.book_id=ac.book_id and c.contact_id=ac.contact_id and book_name=?";
-	       readContactPreparedStatement=connection.prepareStatement(sql);
-	    } catch (Exception e) {
-	        throw new DBException(e.getMessage());
-	    }
-	}
-	public List<PersonContact> readConatctsAddedInRange(Date startDate, Date endDate) {
-        if (contactAddedGivenRangeStatement == null) {
-            this.preparedStatementToretriveContactsInRange();
-        }
-        try {
-        	contactAddedGivenRangeStatement.setDate(1, startDate);
-        	contactAddedGivenRangeStatement.setDate(2, endDate);
-            ResultSet resultSet = contactAddedGivenRangeStatement.executeQuery();
-            return this.getContactList(resultSet);
-        } catch (Exception e) {
-            throw new DBException(e.getMessage());
-        }
-    }
-		private void preparedStatementToretriveContactsInRange() {
-        try {
-            Connection connection = this.getConnection();
-            String query = "SELECT * from contact_details where date_added between ? and ?";
-            contactAddedGivenRangeStatement = connection.prepareStatement(query);
-        } catch (Exception e) {
-            throw new DBException(e.getMessage());
-        }
-    }
-		public List<PersonContact> readContactsInGivenCityOrState(String city, String state) {
-			if (contactsInGivenCityOrStateStatement == null) {
-				this.preparedStatementToretriveContactsInGivenCityOrState();
-			}
-			try {
-				contactsInGivenCityOrStateStatement.setString(1, city);
-				contactsInGivenCityOrStateStatement.setString(2, state);
-				ResultSet resultSet = contactsInGivenCityOrStateStatement.executeQuery();
-				return this.getContactList(resultSet);
-			} catch (Exception e) {
-				throw new DBException(e.getMessage());
-			}
-		}
-		private void preparedStatementToretriveContactsInGivenCityOrState() {
-			try {
-				Connection connection = this.getConnection();
-				String query = "SELECT * from contact_details c ,place p where c.place_id = p.place_id and city =? or state=?";
-				contactsInGivenCityOrStateStatement = connection.prepareStatement(query);
-			} catch (Exception e) {
-				throw new DBException(e.getMessage());
-			}
-		}
 
+	private void prepareStatementForContactData() {
+
+		try {
+			Connection connection = this.getConnection();
+			String sqlStatement = "SELECT * FROM contacts WHERE firstName = ?;";
+			addressDataStatement = connection.prepareStatement(sqlStatement);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
