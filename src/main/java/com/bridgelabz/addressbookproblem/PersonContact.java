754b6420e5c84766bc7f8c98815cc984abe9cbe2
namancg <naman.c@perfios.com>
2021-09-30 23:31:45 +0530

    REFACTORED: constructors

--- src/main/java/com/bridgelabz/addressbookproblem/PersonContact.java
+++ src/main/java/com/bridgelabz/addressbookproblem/PersonContact.java
@@ -1,35 +1,65 @@
 package com.bridgelabz.addressbookproblem;
 
+import java.util.HashMap;
+
 import com.opencsv.bean.CsvBindByName;
 
 public class PersonContact {
 	//private String firstName, lastName, email;
-	private long number;
-	private String address;
+	public int getContactId() {
+		return contactId;
+	}
+	public void setContactId(int contactId) {
+		this.contactId = contactId;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-	@CsvBindByName(column = "First Name")
+	@CsvBindByName
+	private int contactId;
+	@CsvBindByName
 	private String firstName;
-	
-	@CsvBindByName(column = "Last Name")
+	@CsvBindByName
 	private String lastName;
-	
-	@CsvBindByName(column = "Email")
-	private String email;
-	
-	@CsvBindByName(column = "Phone Number")
-	private long phoneNumber;
-	
-	@CsvBindByName(column = "City")
+	@CsvBindByName
+	private String address;
+	@CsvBindByName
 	private String city;
-	
-	@CsvBindByName(column = "State")
+	@CsvBindByName
 	private String state;
-	
-	@CsvBindByName(column = "Zip Code")
-	private long zip;
+	@CsvBindByName
+	private int zip;
+	@CsvBindByName
+	private String phoneNumber;
+	@CsvBindByName
+	private String email;
 	Place place;
-	private Integer contactId;
-	PersonContact(Integer contactId,String firstName, String lastName, String city, String address, String state, int zip, int phoneNumber,
+	PersonContact(String firstName, String lastName, String city, String address, String state, int zip, String phoneNumber,
+			String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		place=new Place(city, zip, state);
+	}
+	public Place getPlace() {
+		return place;
+	}
+	public void setPlace(Place place) {
+		this.place = place;
+	}
+	PersonContact(Integer contactId,String firstName, String lastName, String city, String address, String state, int zip,String phoneNumber,
 			String email ,Integer placeId) {
 		this.contactId=contactId;
 		this.firstName = firstName;
@@ -39,92 +69,92 @@ public class PersonContact {
 		this.email = email;
 		place=new Place(placeId,city,zip,state);
 	}
+
 	public PersonContact() {
 		// TODO Auto-generated constructor stub
 	}
 	public String getFirstName() {
 		return firstName;
 	}
-	
-	public String getLastName() {
-		return lastName;
-	}
-	
-	public long getPhoneNumber() {
-		return number;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public String getAddress() {
-		return address;
-	}
-	
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	
+
+	public String getLastName() {
+		return lastName;
+	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
-	public void setnumber(long number) {
-		this.number = number;
+
+	public String getAddress() {
+		return address;
 	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
 
-	public String toString() {
-		
-		return "Person Details:\nFirst Name:  "+firstName+" \nLast Name : "+lastName+"\nPhone Number : "+number+"\nEmail :"+email+"\nAddress : "+address;
-	}
-	public long getZip() {
-		// TODO Auto-generated method stub
-		return zip;
-	}
-	public Place getPlace() {
-		return place;
-	}
-	public void setPlace(Place place) {
-		this.place = place;
-	}
 	public String getCity() {
-		return getPlace().getCity();
+		return city;
 	}
 
 	public void setState(String state) {
-		this.getPlace().setState(state);
+		this.state=state;
 	}
 
 	public String getState() {
-		return getPlace().getState();
+		return state;
 	}
 
 	public void setCity(String city) {
-		this.getPlace().setCity(city);
+		this.city=city;
 	}
 
 	public int getZipCode() {
-		return getPlace().getZip();
+		return zip;
 	}
 
 	public void setZipCode(int zip) {
-		this.getPlace().setZip(zip); 
+		this.zip=zip; 
 	}
-	public int getContactId() {
-		return contactId;
+
+	public String getPhoneNumber() {
+		return phoneNumber;
 	}
-	public void setContactId(int contactId) {
-		this.contactId = contactId;
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
+
+	public String getEmailId() {
+		return email;
+	}
+
+	public void setEmailId(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public boolean equals(Object anotherObject) {
+		@SuppressWarnings("unchecked")
+		HashMap<String, PersonContact> addressBook = (HashMap<String, PersonContact>) anotherObject;
+		if (addressBook.keySet().stream().anyMatch(s -> (s.equals(firstName)))) {
+			return true;
+		}
+		return false;
+
+	}
+
+	@Override
+	public String toString() {
+		return "First Name: " + getFirstName() + "\n" + "Last Name: " + getLastName() + "\n" + "city :" + getCity()
+				+ "\n" + "Address: " + getAddress() + "\n" + "state: " + getState() + "\n" + "Phone Number: "
+				+ getPhoneNumber() + "\n" + "Email: " + getEmailId();
+	}
+
 
 	
 	}
